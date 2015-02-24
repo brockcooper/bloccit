fc76a213cc4ab8bd10ddb332d658cf98e697f2dd
Brock Cooper <brockcooper@gmail.com>
2015-02-24 07:53:05 -0700

    Start adding Voting

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   resources :users, only: [:update]
   
   resources :topics do
-     resources :posts, except: [:index]
+    resources :posts, except: [:index]
    end
  
-   resources :posts, only: [] do
-     resources :comments, only: [:create, :destroy]
-   end
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
+  end
 
   get 'about' => 'welcome#about'
   get 'contact' => 'welcome#contact'
