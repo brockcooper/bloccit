44bd7868127f6d97a6741f664796832bfc650068
Brock Cooper <brockcooper@gmail.com>
2015-03-02 07:48:00 -0700

    Finish interlude assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,29 @@ module ApplicationHelper
    end
  end
 
-def markdown_to_html(markdown)
-  renderer = Redcarpet::Render::HTML.new
-  extensions = {fenced_code_blocks: true}
-  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-  (redcarpet.render markdown).html_safe
-end
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
+  def up_vote_link_classes(post)
+    if current_user.voted(post) && current_user.voted(post).up_vote?
+      voted = 'voted'
+    else
+      voted = ''
+    end
+    "glyphicon glyphicon-chevron-up #{voted}"
+  end
+
+  def down_vote_link_classes(post)
+    if current_user.voted(post) && current_user.voted(post).down_vote?
+      voted = 'voted'
+    else
+      voted = ''
+    end
+    "glyphicon glyphicon-chevron-up #{voted}"
+  end
 
 end
